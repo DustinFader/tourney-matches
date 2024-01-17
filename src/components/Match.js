7247d9f91927478d2136a876fe486d31a24c3f03
DustinFader <dustinfader16@gmail.com>
2024-01-17 15:54:52 -0400

    comment prop argument effects for now

--- src/components/Match.js
+++ src/components/Match.js
@@ -3,8 +3,8 @@ import React from "react";
 function Match(props) {
   return (
     <article className="Match">
-      <h1>{ props.players[0] } <span>Vs</span> { props.players[1] }</h1>
-      { props.winner ? <h2>{ props.winner } is the winner by 1!</h2> : <h2>No winners yet!</h2>}
+      <h1>{ /*props.players[0] */ } <span>Vs</span> { /* props.players[1] */ }</h1>
+      { /* props.winner ? <h2>{ props.winner } is the winner by 1!</h2> : <h2>No winners yet!</h2> */}
     </article>
   )
 }
