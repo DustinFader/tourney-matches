60500016143bc0b8d8a4b66db8d9522fbfa2cfaa
DustinFader <dustinfader16@gmail.com>
2024-01-17 14:20:10 -0400

    add section that lists the matches

---
+++ src/components/MatchList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Match from "./Match";
+
+function MatchList() {
+  return (
+    <section>
+      <h1>Current Match List</h1>
+      <Match />
+    </section>
+  );
+}
+
+export default MatchList;
