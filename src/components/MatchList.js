c98b92f255a20c8af7dd1af03c6df29d405c6319
DustinFader <dustinfader16@gmail.com>
2024-01-22 20:07:08 -0400

    render single match

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,10 +3,12 @@ import matchData from "../data/matchData";
 import Match from "./Match";
 
 function MatchList(props) {
+  const oneMatch = matchData[0];
+
   return (
-    <section>
-      <h1>Current Match List</h1>
-      <Match />
+    <section className="PlayerList MatchList">
+      <h1>Match List</h1>
+      <Match Players={oneMatch.players} winners={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   );
 }
