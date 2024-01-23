d7adcae36e4e900217a6987c5a7c7737fe819df0
DustinFader <dustinfader16@gmail.com>
2024-01-22 21:11:44 -0400

    move player and match data imports to app.js

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,14 +1,13 @@
 import React from "react";
-import matchData from "../data/matchData";
 import Match from "./Match";
 
 function MatchList(props) {
-  const oneMatch = matchData[0];
+  const oneMatch = props.matchData[0];
 
   return (
     <section className="PlayerList MatchList">
       <h1>Match List</h1>
-      <Match Players={oneMatch.players} winners={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
+      <Match Players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   );
 }
