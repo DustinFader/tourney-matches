fad94c834d4f1123992bbb28be3c3c2e1a8fd67f
DustinFader <dustinfader16@gmail.com>
2024-01-22 20:57:43 -0400

    add score difference to match

--- src/components/Match.js
+++ src/components/Match.js
@@ -4,7 +4,7 @@ function Match(props) {
   return (
     <article className="Match">
       <h1>{ props.Players[0] } <span>Vs</span> { props.Players[1] }</h1>
-      { props.winners ? <h2>{ props.winners } is the winner by 1!</h2> : <h2>No winners yet!</h2>}
+      { props.winner ? <h2>{ props.winner } is the winner by {props.scoreDifference}!</h2> : <h2>No winners yet!</h2>}
     </article>
   )
 }
